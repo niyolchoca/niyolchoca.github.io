4e2a26f35115484d60bc5bf1e5013f625d706af6
rrrrodrrrrigo <yectocsalev"gmail.com>
2020-02-19 21:09:09 -0500

    Nitlayocoya

--- xochitl.js
+++ xochitl.js
@@ -490,7 +490,7 @@ function tecihuatl() {
     var ruidoC = 0.00008;
     var posC = noise(millis() * ruidoC) * 12;
 
-    translate(-60, 200);
+    translate(-200, 200);
 
     //    ton_xspacing = 4; // How far apart should each horizontal position be spaced
 
